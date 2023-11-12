72d570b2bc7c50b8b0d8c036f86db1ae2e1efd65
Uniwerty <ivcdim@yandex.ru>
2023-11-12 17:50:27 +0300

    Make initial schema for product-engine database

---
+++ product-engine/src/main/resources/db/changelog/001-initial-schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE products
+(
+    product_code           varchar PRIMARY KEY,
+    min_term               int     NOT NULL,
+    max_term               int     NOT NULL,
+    min_interest           numeric NOT NULL,
+    max_interest           numeric NOT NULL,
+    min_principal_amount   numeric NOT NULL,
+    max_principal_amount   numeric NOT NULL,
+    min_origination_amount numeric NOT NULL,
+    max_origination_amount numeric NOT NULL
+);
+INSERT INTO products
+VALUES ('CL-1.0', 3, 24, '8', '15', '50000', '500000', '2000', '10000');
+CREATE TABLE agreements
+(
+    agreement_id       varchar PRIMARY KEY,
+    product_code       varchar REFERENCES products,
+    client_id          varchar NOT NULL,
+    status             varchar CHECK ( status in ('NEW', 'ACTIVE', 'CLOSED') ),
+    loan_term          int     NOT NULL,
+    interest           numeric NOT NULL,
+    principal_amount   numeric NOT NULL,
+    origination_amount numeric NOT NULL,
+    disbursement_date  date,
+    next_payment_date  date
+);
+CREATE TABLE payment_schedules
+(
+    schedule_id  varchar PRIMARY KEY,
+    agreement_id varchar REFERENCES agreements,
+    version      int NOT NULL
+);
+CREATE TABLE payments
+(
+    schedule_id       varchar REFERENCES payment_schedules,
+    status            varchar CHECK ( status in ('PAID', 'OVERDUE', 'FUTURE') ),
+    payment_date      date,
+    period_payment    numeric,
+    interest_payment  numeric,
+    principal_payment numeric,
+    period_number     int
+);
