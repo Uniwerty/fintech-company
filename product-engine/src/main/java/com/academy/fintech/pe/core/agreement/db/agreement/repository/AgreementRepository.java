679eaf2776cb8230e8b2a2eceee5c91ec6780afa
Uniwerty <ivcdim@yandex.ru>
2023-11-19 23:28:13 +0300

    Make AgreementRepository update method transactional

--- product-engine/src/main/java/com/academy/fintech/pe/core/agreement/db/agreement/repository/AgreementRepository.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/agreement/db/agreement/repository/AgreementRepository.java
@@ -1,6 +1,7 @@
 package com.academy.fintech.pe.core.agreement.db.agreement.repository;
 
 import com.academy.fintech.pe.core.agreement.db.agreement.model.Agreement;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Repository
 public interface AgreementRepository extends CrudRepository<Agreement, UUID> {
     @Modifying
+    @Transactional
     @Query(value = """
             UPDATE agreements SET
             next_payment_date = :date
