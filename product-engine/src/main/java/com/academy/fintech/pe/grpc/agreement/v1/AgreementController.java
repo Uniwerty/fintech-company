deca34dabf562c4abe7ae44782716afdf15a39c2
Uniwerty <ivcdim@yandex.ru>
2023-11-19 18:27:44 +0300

    Add logging to AgreementController

--- product-engine/src/main/java/com/academy/fintech/pe/grpc/agreement/v1/AgreementController.java
+++ product-engine/src/main/java/com/academy/fintech/pe/grpc/agreement/v1/AgreementController.java
@@ -10,6 +10,8 @@ import com.academy.fintech.pe.core.agreement.service.AgreementCreationService;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import org.lognet.springboot.grpc.GRpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
@@ -19,30 +21,37 @@ public class AgreementController extends AgreementServiceGrpc.AgreementServiceIm
     private final AgreementMapper agreementMapper;
     private final AgreementCreationService agreementCreationService;
     private final AgreementSchedulingService agreementSchedulingService;
+    private final Logger logger = LoggerFactory.getLogger(AgreementController.class);
 
     @Override
     public void create(AgreementCreationRequest request,
                        StreamObserver<AgreementCreationResponse> responseObserver) {
+        logger.info("Agreement creation request for client {} received", request.getClientId());
         UUID createdAgreementId =
                 agreementCreationService.create(agreementMapper.mapCreationRequestToDto(request));
+        logger.info("New agreement created successfully");
         responseObserver.onNext(
                 AgreementCreationResponse.newBuilder()
                         .setAgreementId(createdAgreementId.toString())
                         .build()
         );
         responseObserver.onCompleted();
+        logger.info("Agreement creation response sent");
     }
 
     @Override
     public void activate(AgreementActivationRequest request,
                          StreamObserver<AgreementActivationResponse> responseObserver) {
+        logger.info("Agreement {} activation request received", request.getAgreementId());
         UUID createdScheduleId =
                 agreementSchedulingService.activate(agreementMapper.mapActivationRequestToDto(request));
+        logger.info("Agreement activated successfully");
         responseObserver.onNext(
                 AgreementActivationResponse.newBuilder()
                         .setScheduleId(createdScheduleId.toString())
                         .build()
         );
         responseObserver.onCompleted();
+        logger.info("Agreement activation response sent");
     }
 }
