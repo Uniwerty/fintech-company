70392b1e675b5c6f764cc119eb62341672baebf5
Uniwerty <ivcdim@yandex.ru>
2023-11-23 00:53:36 +0300

    Add application and client DTOs

---
+++ origination/src/main/java/com/academy/fintech/origination/public_interface/application/dto/ApplicationDto.java
@@ -0,0 +1,16 @@
+package com.academy.fintech.origination.public_interface.application.dto;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.UUID;
+
+@Builder
+public record ApplicationDto(
+        UUID clientId,
+        BigDecimal requiredAmount,
+        String status,
+        Date creationDate
+) {
+}
