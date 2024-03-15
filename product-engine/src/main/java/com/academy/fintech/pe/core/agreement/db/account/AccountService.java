ae80b4f8467ecd8741faebf38fe9e3449fda0a2a
Uniwerty <ivcdim@yandex.ru>
2024-03-15 23:20:57 +0300

    Update AccountRepository and AccountService with logic to pay for period

--- product-engine/src/main/java/com/academy/fintech/pe/core/agreement/db/account/AccountService.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/agreement/db/account/AccountService.java
@@ -3,10 +3,12 @@ package com.academy.fintech.pe.core.agreement.db.account;
 import com.academy.fintech.pe.core.agreement.db.account.model.Account;
 import com.academy.fintech.pe.core.agreement.db.account.model.AccountCode;
 import com.academy.fintech.pe.core.agreement.db.account.repository.AccountRepository;
+import com.academy.fintech.pe.core.schedule.status.PaymentStatus;
 import com.academy.fintech.pe.public_interface.agreement.dto.AgreementPaymentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
@@ -28,6 +30,38 @@ public class AccountService {
                 agreementPaymentDto.agreementId(),
                 AccountCode.STANDARD.name(),
                 agreementPaymentDto.amount()
+        );
+    }
+
+    public BigDecimal getAmount(UUID agreementId) {
+        return accountRepository
+                .getAccountByAgreementIdAndAccountCode(
+                        agreementId,
+                        AccountCode.STANDARD.name()
+                )
+                .map(Account::getBalance)
+                .orElseThrow();
+    }
+
+    public void payForPeriodFully(UUID agreementId, BigDecimal paymentAmount, UUID paymentId) {
+        accountRepository.payForPeriodFully(
+                agreementId,
+                paymentAmount,
+                paymentId,
+                PaymentStatus.PAID.name()
+        );
+    }
+
+    public void payForPeriodPartially(UUID agreementId,
+                                      BigDecimal paymentAmount,
+                                      BigDecimal overdueAmount,
+                                      UUID paymentId) {
+        accountRepository.payForPeriodPartiallyAndAccrueOverdue(
+                agreementId,
+                paymentAmount,
+                overdueAmount,
+                paymentId,
+                PaymentStatus.OVERDUE.name()
         );
     }
 }
