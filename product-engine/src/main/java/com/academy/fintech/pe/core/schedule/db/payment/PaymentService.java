a4d7031ce0ff72c5f32b6d8779c5b4b7e600ae95
Uniwerty <ivcdim@yandex.ru>
2024-03-15 23:19:27 +0300

    Update PaymentRepository and PaymentService with logic to find period payments

--- product-engine/src/main/java/com/academy/fintech/pe/core/schedule/db/payment/PaymentService.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/schedule/db/payment/PaymentService.java
@@ -1,11 +1,16 @@
 package com.academy.fintech.pe.core.schedule.db.payment;
 
+import com.academy.fintech.pe.core.schedule.db.payment.model.Payment;
 import com.academy.fintech.pe.core.schedule.db.payment.repository.PaymentRepository;
+import com.academy.fintech.pe.core.schedule.status.PaymentStatus;
 import com.academy.fintech.pe.public_interface.payment.dto.PaymentDto;
+import com.academy.fintech.pe.public_interface.payment.dto.PeriodPaymentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +23,23 @@ public class PaymentService {
                         .map(PaymentMapper::mapDtoToEntity)
                         .toList()
         );
+    }
+
+    public PeriodPaymentDto getPeriodPaymentByScheduleIdAndDate(UUID scheduleId, Date paymentDate) {
+        return paymentRepository
+                .findPaymentByScheduleIdAndStatusAndPaymentDate(
+                        scheduleId,
+                        PaymentStatus.FUTURE.name(),
+                        paymentDate
+                )
+                .map(PaymentMapper::mapEntityToPeriodPaymentDto)
+                .orElseThrow();
+    }
+
+    public Date getPaymentDateByScheduleIdAndPeriodNumber(UUID scheduleId, int periodNumber) {
+        return paymentRepository
+                .findPaymentByScheduleIdAndPeriodNumber(scheduleId, periodNumber)
+                .map(Payment::getPaymentDate)
+                .orElse(null);
     }
 }
--- product-engine/src/main/java/com/academy/fintech/pe/core/schedule/db/payment/repository/PaymentRepository.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/schedule/db/payment/repository/PaymentRepository.java
@@ -1,11 +1,21 @@
 package com.academy.fintech.pe.core.schedule.db.payment.repository;
 
 import com.academy.fintech.pe.core.schedule.db.payment.model.Payment;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends CrudRepository<Payment, UUID> {
+    Optional<Payment> findPaymentByScheduleIdAndStatusAndPaymentDate(UUID scheduleId,
+                                                                     String status,
+                                                                     Date paymentDate);
+
+    Optional<Payment> findPaymentByScheduleIdAndPeriodNumber(UUID scheduleId, int periodNumber);
 }
