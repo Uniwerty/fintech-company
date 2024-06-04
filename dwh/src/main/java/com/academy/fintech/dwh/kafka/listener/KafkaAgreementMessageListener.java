c0b8e86b210d110f0e04502b8f8c132df1800ba3
Uniwerty <ivcdim@yandex.ru>
2024-06-04 16:59:08 +0300

    Add try-finally blocks and logging to kafka listeners

--- dwh/src/main/java/com/academy/fintech/dwh/kafka/listener/KafkaAgreementMessageListener.java
+++ dwh/src/main/java/com/academy/fintech/dwh/kafka/listener/KafkaAgreementMessageListener.java
@@ -3,11 +3,13 @@ package com.academy.fintech.dwh.kafka.listener;
 import com.academy.fintech.dwh.core.agreement.db.AgreementService;
 import com.academy.fintech.dwh.public_interface.agreement.AgreementDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaAgreementMessageListener {
@@ -19,7 +21,11 @@ public class KafkaAgreementMessageListener {
             containerFactory = "agreementContainerFactory"
     )
     public void getAgreementMessage(ConsumerRecord<String, AgreementDto> record, Acknowledgment ack) {
-        agreementService.save(record.value());
-        ack.acknowledge();
+        try {
+            agreementService.save(record.value());
+            log.info("Received agreement message: {}", record);
+        } finally {
+            ack.acknowledge();
+        }
     }
 }
--- dwh/src/main/java/com/academy/fintech/dwh/kafka/listener/KafkaApplicationMessageListener.java
+++ dwh/src/main/java/com/academy/fintech/dwh/kafka/listener/KafkaApplicationMessageListener.java
@@ -3,11 +3,13 @@ package com.academy.fintech.dwh.kafka.listener;
 import com.academy.fintech.dwh.core.application.db.ApplicationService;
 import com.academy.fintech.dwh.public_interface.application.ApplicationDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaApplicationMessageListener {
@@ -19,7 +21,11 @@ public class KafkaApplicationMessageListener {
             containerFactory = "applicationContainerFactory"
     )
     public void getApplicationMessage(ConsumerRecord<String, ApplicationDto> record, Acknowledgment ack) {
-        applicationService.save(record.value());
-        ack.acknowledge();
+        try {
+            applicationService.save(record.value());
+            log.info("Received application message: {}", record);
+        } finally {
+            ack.acknowledge();
+        }
     }
 }
